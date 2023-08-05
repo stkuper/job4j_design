47b9bf5f0ddbd68982e59f055c44071963c8494f
stkuper <stklipper@gmail.com>
2023-08-05 13:52:42 +0300

    5. Утверждения с коллекциями [#504885  [#504885]]

---
+++ src/test/java/ru/job4j/assertj/SimpleConvertTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.assertj;
+
+import org.assertj.core.data.Index;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SimpleConvertTest {
+    @Test
+    void checkArray() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        String[] array = simpleConvert.toArray("first", "second", "three", "four", "five");
+        assertThat(array).hasSize(5)
+                .contains("second")
+                .contains("first", Index.atIndex(0))
+                .containsAnyOf("zero", "second", "six")
+                .doesNotContain("first", Index.atIndex(1));
+    }
+
+    @Test
+    void checkList() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        List<String> list = simpleConvert.toList("first", "second", "three", "four", "five");
+        assertThat(list).hasSize(5)
+                .contains("second")
+                .contains("first", Index.atIndex(0))
+                .containsAnyOf("zero", "second", "six")
+                .doesNotContain("first", Index.atIndex(1));
+    }
+
+    @Test
+    void checkSet() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Set<String> set = simpleConvert.toSet("first", "second", "three", "four", "five", "second", "four");
+        assertThat(set).hasSize(5)
+                .contains("second")
+                .containsOnly("first", "second", "three", "four", "five")
+                .containsAnyOf("zero", "second", "six")
+                .doesNotContain("six", "zero", "seven");
+    }
+
+    @Test
+    void checkMap() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Map<String, Integer> map = simpleConvert.toMap("first", "second", "three", "four", "five", "second", "four");
+        assertThat(map).hasSize(5)
+                .containsKeys("second", "five")
+                .containsValues(1, 2, 4, 3, 5)
+                .doesNotContainKeys("six", "zero", "seven")
+                .doesNotContainValue(0)
+                .containsEntry("four", 4);
+    }
+}
