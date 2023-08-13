47ccf455c52fbcf0c9f3238db1a601da2ddc98b4
stkuper <stklipper@gmail.com>
2023-08-13 11:14:44 +0300

    6. Перевернуть связанный список [#161 #436382]

--- src/main/java/ru/job4j/collection/RevertLinked.java
+++ src/main/java/ru/job4j/collection/RevertLinked.java
@@ -20,7 +20,8 @@ public class RevertLinked<T> implements Iterable<T> {
     }
 
     public boolean revert() {
-        if (head != null && head.next != null) {
+        boolean result = head != null && head.next != null;
+        if (result) {
             Node<T> current = head.next;
             head.next = null;
             while (current != null) {
@@ -30,7 +31,7 @@ public class RevertLinked<T> implements Iterable<T> {
                 current = next;
             }
         }
-        return head != null && head.next != null;
+        return result;
     }
 
     @Override
