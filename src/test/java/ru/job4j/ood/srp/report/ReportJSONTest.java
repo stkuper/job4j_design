cec13b510f7bca67622e561c4b44af8225e2c283
stkuper <stklipper@gmail.com>
2024-04-06 20:53:41 +0300

    1. Поддержка форматов XML, JSON в генераторе отчетов. [#851]

--- src/test/java/ru/job4j/ood/srp/report/ReportJSONTest.java
+++ src/test/java/ru/job4j/ood/srp/report/ReportJSONTest.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ReportJSONTest {
     @Test
-    void whenReportJSON() {
+    void whenReportJSONSingle() {
         Store store = new MemoryStore();
         Calendar calendar = Calendar.getInstance();
         DateTimeParser<Calendar> dateTimeParser = new ReportDateTimeParser();
@@ -25,6 +25,38 @@ class ReportJSONTest {
                           + "    \"name\": \"Stanislav\",\n"
                           + "    \"hired\": \"" + dateTimeParser.parse(calendar) + "\",\n"
                           + "    \"salary\": 70000.0\n"
+                          + "  }\n"
+                          + "]";
+        assertThat(reportJSON.generate(emp -> true)).isEqualTo(expected);
+    }
+
+    @Test
+    void whenReportJSONMany() {
+        Store store = new MemoryStore();
+        Calendar calendar = Calendar.getInstance();
+        DateTimeParser<Calendar> dateTimeParser = new ReportDateTimeParser();
+        store.add(new Employee("Stanislav", calendar, calendar, 70000));
+        store.add(new Employee("Kirill", calendar, calendar, 85000));
+        store.add(new Employee("Alexey", calendar, calendar, 100000));
+        ReportJSON reportJSON = new ReportJSON(store, new ReportDateTimeParser());
+        String expected = "[\n"
+                          + "  {\n"
+                          + "    \"fired\": \"" + dateTimeParser.parse(calendar) + "\",\n"
+                          + "    \"name\": \"Stanislav\",\n"
+                          + "    \"hired\": \"" + dateTimeParser.parse(calendar) + "\",\n"
+                          + "    \"salary\": 70000.0\n"
+                          + "  },\n"
+                          + "  {\n"
+                          + "    \"fired\": \"" + dateTimeParser.parse(calendar) + "\",\n"
+                          + "    \"name\": \"Kirill\",\n"
+                          + "    \"hired\": \"" + dateTimeParser.parse(calendar) + "\",\n"
+                          + "    \"salary\": 85000.0\n"
+                          + "  },\n"
+                          + "  {\n"
+                          + "    \"fired\": \"" + dateTimeParser.parse(calendar) + "\",\n"
+                          + "    \"name\": \"Alexey\",\n"
+                          + "    \"hired\": \"" + dateTimeParser.parse(calendar) + "\",\n"
+                          + "    \"salary\": 100000.0\n"
                           + "  }\n"
                           + "]";
         assertThat(reportJSON.generate(emp -> true)).isEqualTo(expected);
--- src/test/java/ru/job4j/ood/srp/report/ReportXMLTest.java
+++ src/test/java/ru/job4j/ood/srp/report/ReportXMLTest.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ReportXMLTest {
     @Test
-    void whenReportXML() {
+    void whenReportXMLSingle() {
         Store store = new MemoryStore();
         Calendar calendar = Calendar.getInstance();
         DateTimeParser<Calendar> dateTimeParser = new ReportDateTimeParser();
@@ -26,6 +26,39 @@ class ReportXMLTest {
                           + "        <hired>" + dateTimeParser.parse(calendar) + "</hired>\n"
                           + "        <fired>" + dateTimeParser.parse(calendar) + "</fired>\n"
                           + "        <salary>100000.0</salary>\n"
+                          + "    </employee>\n"
+                          + "</employees>\n";
+        assertThat(reportXML.generate(emp -> true)).isEqualTo(expected);
+    }
+
+    @Test
+    void whenReportXMLMany() {
+        Store store = new MemoryStore();
+        Calendar calendar = Calendar.getInstance();
+        DateTimeParser<Calendar> dateTimeParser = new ReportDateTimeParser();
+        store.add(new Employee("Stanislav", calendar, calendar, 100000));
+        store.add(new Employee("Kirill", calendar, calendar, 70000));
+        store.add(new Employee("Alexey", calendar, calendar, 90000));
+        ReportXML reportXML = new ReportXML(store);
+        String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                          + "<employees>\n"
+                          + "    <employee>\n"
+                          + "        <name>Stanislav</name>\n"
+                          + "        <hired>" + dateTimeParser.parse(calendar) + "</hired>\n"
+                          + "        <fired>" + dateTimeParser.parse(calendar) + "</fired>\n"
+                          + "        <salary>100000.0</salary>\n"
+                          + "    </employee>\n"
+                          + "    <employee>\n"
+                          + "        <name>Kirill</name>\n"
+                          + "        <hired>" + dateTimeParser.parse(calendar) + "</hired>\n"
+                          + "        <fired>" + dateTimeParser.parse(calendar) + "</fired>\n"
+                          + "        <salary>70000.0</salary>\n"
+                          + "    </employee>\n"
+                          + "    <employee>\n"
+                          + "        <name>Alexey</name>\n"
+                          + "        <hired>" + dateTimeParser.parse(calendar) + "</hired>\n"
+                          + "        <fired>" + dateTimeParser.parse(calendar) + "</fired>\n"
+                          + "        <salary>90000.0</salary>\n"
                           + "    </employee>\n"
                           + "</employees>\n";
         assertThat(reportXML.generate(emp -> true)).isEqualTo(expected);
